1460260a2ac7813cdb7a8bc8122b59a594e5d718
srph <seraphipod@gmail.com>
2021-03-21 17:37:35 +0800

    Update page titles and site-wide title:

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `A Aesthetics`,
+    description: `Enhancing Looks & Changing Lives. Achieve that body that you deserve.`,
+    author: `@srph`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -6,11 +6,17 @@
  */
 
 import * as React from "react"
-import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-function SEO({ description, lang, meta, title }) {
+interface Props {
+  title: string
+  description?: string
+  lang?: string
+  meta?: string
+}
+
+function SEO({ description = '', lang = 'en', meta = [], title }: Props) {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -71,19 +77,6 @@ function SEO({ description, lang, meta, title }) {
       ].concat(meta)}
     />
   )
-}
-
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
-}
-
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
 }
 
 export { SEO }
