74bb6c729c0d58d5f66eb02e028922f69e350593
srph <seraphipod@gmail.com>
2021-04-07 10:21:08 +0800

    Owner

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -14,6 +14,8 @@ import { FaArrowRight } from '../components/Icon'
 import asset_aboutCertificate1 from '../assets/about-certificate-1.png'
 import asset_aboutCertificate2 from '../assets/about-certificate-2.png'
 import asset_aboutDrRoura from '../assets/about-dr-roura.jpg'
+import asset_aboutOwner from '../assets/about-owner.jpg'
+import asset_aboutOwner2 from '../assets/about-owner-2.jpg'
 
 import asset_mentorDrAlfredoHoyos from '../assets/mentor-dr-alfredo-hoyos.jpg'
 import asset_mentorDrHoyosMillard from '../assets/mentor-hoyos-millard.jpg'
@@ -26,6 +28,18 @@ import asset_mentorDrsRobertMargetWeiss from '../assets/mentor-drs-robert-margar
 import asset_mentorDrSorinEremia from '../assets/mentor-dr-sorin-eremia.jpg'
 
 const roura = {
+  description:
+    'Dr. Claudine S. Roura is an expert in 4DLipo, HD Tummy Tuck Surgery and has been known for her outstanding expertise in VASER High Definition Liposculpture. She is the first and only cosmetic surgeon certified to do VASER High Definition Liposculpture in the Philippines.',
+  body: [
+    'Dr. Roura obtained her degree in medicine at the University of Santo Tomas (UST) Faculty of Medicine and Surgery, took up her residency training in Dermatology at the Jose R. Reyes Memorial Medical Center, After which she did was awarded the Walter de Groot Phlebology fellowship award where she trained with Dr. Robert Weiss in Maryland, USA in November 2005.',
+    'She was also awarded the Dermatologic Surgery Preceptorship Program of the American Society of Dermatologic surgery and was under Dr. Sorin Eremia of Riverside, California USA in July 2003. Locally, she did her Preceptorship in Cosmetic Surgery with Dr. Raul B. Guanzon, prominent cosmetic surgeon in the Philippines.',
+    'Vaser came to the Philippines around 2007 and she was assigned as the Systems Application Specialist for Vaser Philippines. With special interest in the art of body sculpting, she underwent intensive training in High Definition Liposculpture in 2008 and advanced training in 4D Lipo & HD Tummy Tuck in 2014 with Dr. Alfredo Hoyos in Colombia.',
+    'More recent training is art of Body Sculpting is with Dr. Hourglass, Dr. Wilberto Cortes in Houston Texas in March 2018 focusing on Hourglass Lipo and Hourglass Tummy Tack and Brazilian Buttlift.'
+  ]
+}
+
+const owner = {
+  name: 'Elizabeth Fernandez-Gabucan',
   description:
     'Dr. Claudine S. Roura is an expert in 4DLipo, HD Tummy Tuck Surgery and has been known for her outstanding expertise in VASER High Definition Liposculpture. She is the first and only cosmetic surgeon certified to do VASER High Definition Liposculpture in the Philippines.',
   body: [
@@ -200,29 +214,35 @@ const AboutPage = () => (
 
     <Section>
       <Container>
-        <SectionHeading title="Meet the owner" body="Mrs. Fernandez" bodyWeight="bold" />
+        <SectionHeading title="Meet the owner" body={owner.name} bodyWeight="bold" />
       </Container>
 
-      <div className="mb-24 lg:mb-8" />
+      <div className="mb-48 lg:mb-56" />
 
       <Container size="lg">
-        <div className="relative flex flex-col lg: flex-row bg-gold-500">
-          <div className="relative -mt-16 lg:-mt-0 lg:absolute flex px-4 lg:px-0 lg:top-8 lg:-left-8 lg:mb-0">
-            <img src={asset_aboutDrRoura} alt="Photo of Dr. Roura" />
+        <div className="relative p-4 bg-gold-500">
+          <div className="relative flex flex-col -mt-48 lg:flex-row">
+            <div className="py-2 lg:py-0 lg:px-2">
+              <img src={asset_aboutOwner} alt={`Photo of ${owner.name}`} />
+            </div>
+
+            <div className="py-2 lg:py-0 lg:px-2">
+              <img src={asset_aboutOwner2} alt={`Photo of ${owner.name}`} />
+            </div>
           </div>
 
-          <div className="self-end lg:w-2/3 px-8 py-8 lg:px-12 lg:py-16">
+          <div className="px-4 py-8 lg:px-32 lg:py-12">
             <Text as="p" size="2xl" weight="medium" color="white" leading="normal">
-              {roura.description}
+              {owner.description}
             </Text>
           </div>
         </div>
       </Container>
 
-      <div className="mb-8 lg:mb-24" />
+      <div className="mb-8 lg:mb-16" />
 
       <Container size="md">
-        {roura.body.map((p, i) => (
+        {owner.body.map((p, i) => (
           <React.Fragment key={i}>
             <Text as="p" size="xl" color="gold.800" leading="loose">
               {p}
